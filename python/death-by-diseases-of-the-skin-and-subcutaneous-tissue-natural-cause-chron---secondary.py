180fa3d4848fb96ec2705700391b702a736bd787
Martin Chapman <contact@martinchapman.co.uk>
2024-06-05 13:53:58 +0100

    Update made by Phenoflow

---
+++ python/death-by-diseases-of-the-skin-and-subcutaneous-tissue-natural-cause-chron---secondary.py
@@ -0,0 +1,25 @@
+# John, A, McGregor, J., Jones, I., Lee, S. C., Walters, J. T. R., Owen, M. J., O'Donovan, M., DelPozo-Banos, M., Berridge, D., and Lloyd, K., 2024.
+
+import sys, csv, re
+
+codes = [{"code":"L53.3","system":"icd10"},{"code":"L41.1","system":"icd10"},{"code":"L28.1","system":"icd10"},{"code":"L57.5","system":"icd10"},{"code":"L58.1","system":"icd10"},{"code":"L57.2","system":"icd10"},{"code":"L57.8","system":"icd10"},{"code":"L28.0","system":"icd10"},{"code":"L57.4","system":"icd10"},{"code":"L57.0","system":"icd10"},{"code":"L90.4","system":"icd10"},{"code":"L28","system":"icd10"},{"code":"L57","system":"icd10"},{"code":"L57.9","system":"icd10"},{"code":"L98.4","system":"icd10"},{"code":"L57.1","system":"icd10"},{"code":"L57.3","system":"icd10"},{"code":"L12.2","system":"icd10"},{"code":"L28.2","system":"icd10"}];
+REQUIRED_CODES = 1;
+with open(sys.argv[1], 'r') as file_in, open('death-by-diseases-of-the-skin-and-subcutaneous-tissue-potential-cases.csv', 'w', newline='') as file_out:
+    csv_reader = csv.DictReader(file_in)
+    csv_writer = csv.DictWriter(file_out, csv_reader.fieldnames + ["death-by-diseases-of-the-skin-and-subcutaneous-tissue-natural-cause-chron---secondary-identified"])
+    csv_writer.writeheader();
+    codes_identified = 0;
+    for row in csv_reader:
+        newRow = row.copy();
+        for cell in row:
+            # Iterate cell lists (e.g. codes)
+            for item in re.findall(r'\(([^,]*)\,', row[cell]):
+                if(item in list(map(lambda code: code['code'], codes))): codes_identified+=1;
+                if(codes_identified>=REQUIRED_CODES):
+                    newRow["death-by-diseases-of-the-skin-and-subcutaneous-tissue-natural-cause-chron---secondary-identified"] = "CASE";
+                    break;
+            if(codes_identified>=REQUIRED_CODES): break;
+        if(codes_identified<REQUIRED_CODES):
+            newRow["death-by-diseases-of-the-skin-and-subcutaneous-tissue-natural-cause-chron---secondary-identified"] = "UNK";
+        codes_identified=0;
+        csv_writer.writerow(newRow)
